f376b80775a2d5f200be2d5bdaae05adfded55d1
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-03-09 12:08:31 -0300

    Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-
-import {   useState } from 'react';
-import './App.css';
-import { productList as initialProducts} from './components/data/data';
-import { Header } from './components/Header';
-import { useFilters } from './hooks/useFilters';
-import { Cart } from './components/Cart';
-import { CartProvider } from './context/cart';
-import { HomeProducts } from './pages/HomeProducts';
-import { Footer } from './components/Footer';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
-
-
+import { useState } from "react";
+import "./App.css";
+import { productList as initialProducts } from "./components/data/data";
+import { Header } from "./components/Header";
+import { useFilters } from "./hooks/useFilters";
+import { Cart } from "./components/Cart";
+import { CartProvider } from "./context/cart";
+import { HomeProducts } from "./pages/HomeProducts";
+import { Footer } from "./components/Footer";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [productList] = useState(initialProducts) 
-  const {filterProducts} = useFilters()
-  const filteredProducts = filterProducts(productList)
-  
+  const [productList] = useState(initialProducts);
+  const { filterProducts } = useFilters();
+  const filteredProducts = filterProducts(productList);
+
   return (
     <BrowserRouter>
       <CartProvider>
-      <Header />
+        <Header />
         <Routes>
-          <Route  path="/*" element = { <Navigate to='/' replace /> }/>
+          <Route path="/*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<HomeProducts products={filteredProducts} />} />
         </Routes>
-      <Cart />
-      <HomeProducts products={filteredProducts} />
-      <Footer />
-    </CartProvider>
+        <Cart />
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
   );
 }
